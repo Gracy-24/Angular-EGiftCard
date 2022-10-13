39600069397eeb3aff7195a8901f072cf5c71cc0
MuskanMaheshwari14 <muskan.maheshwari@capgemini.com>
2022-10-13 10:50:13 +0530

    Some updation

--- EGiftCard/src/app/app-routing.module.ts
+++ EGiftCard/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { GiftCardsComponent } from './component/gift-cards/gift-cards.component';
 import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'gift-cards',component:GiftCardsComponent},
   {path:'about',component:AboutComponent},
+  {path:'login',component:LoginComponent},
   {path:'contact',component:ContactComponent}
 ];
 
